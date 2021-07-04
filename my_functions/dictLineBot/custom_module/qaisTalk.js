a31adc09096918d9b5276b3e6f34238033c6b168
githubmhjao <ntumhjao@gmail.com>
2021-07-04 19:11:29 +0800

    Update qaisTalk.js

--- my_functions/dictLineBot/custom_module/qaisTalk.js
+++ my_functions/dictLineBot/custom_module/qaisTalk.js
@@ -9,7 +9,7 @@ const defaultTalk = () => {
   return response;
 }
 
-const dictTalk = (event) => {
+const dictTalk = async (event) => {
   // fetch data
   const targetWord = event.message.text.trim().toLowerCase();
   const url = `https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/${targetWord}`;
